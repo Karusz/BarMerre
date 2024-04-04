75451478075d013ce513d231b6e9f0dbef8f96b2
Karusz <112902245+Karusz@users.noreply.github.com>
2024-04-04 14:42:05 +0200

    test create

--- test.php
+++ test.php
@@ -5,13 +5,20 @@
     $talalt = $conn->query($lekerd);
     $user = $talalt->fetch_assoc();
 
-    $from = "Budapest, Magyarország";
-	$to = "Miskolc, Magyarország";
+    $from = "";
+	$to = "";
 
 	if(isset($_POST['caclr'])){
 		$from = $_POST['from'];
 		$to = $_POST['to'];
 	}
+
+    if(isset($_POST['save-btn'])){
+        $from = $_GET['from'];
+        $to = $_GET['to'];
+
+        $conn->query("INSERT INTO routes VALUES(id, $_SESSION[userid], 'test', '$from;$to', 'test routes')");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -65,11 +72,14 @@
     </header>
     <div class="buborek">
 
-        <form method="post" action="createroute.php">
+        <form method="post" action="test.php">
 			<input type="text" name="from" id="from" placeholder="From" value="<?php if(!empty($from)){echo $from;} ?>">
 			<input type="text" name="to" id="to" placeholder="To" value="<?php if(!empty($to)){echo $to;} ?>">
 			<button id="calc" name="caclr">Tervezés</button>
 		</form>
+        <form action="test.php?from=<?=$from?>&to=<?= $to?>" method="post">
+            <button class="btn btn-primary" name="save-btn">Mentés</button>
+        </form>
 		<div id="map"></div>
 		<div id="output"></div>
         
@@ -88,73 +98,12 @@
 
 
     <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
-    <script src="assets/js/main.js"></script>
+    
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpybVWsR30eThdM_LVqdGelbyDSlGlBf8&libraries=places&callback=initMap"></script>
-    <script>
-        function initMap(){
-	
-    var location = {lat: 47.50356186982569, lng: 19.085582763138646};
-    
-    var mapOptions = {
-        center: location,
-        zoom: 7
-    };
-    
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    
-    var directionsService = new google.maps.DirectionsService();
-    
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    
-    directionsDisplay.setMap(map);
-    
-    function calcRoute(){
-        
-        var request = {
-            origin: document.getElementById("from").value,
-            destination: document.getElementById("to").value,
-            travelMode: google.maps.TravelMode.WALKING,
-            unitSystem: google.maps.UnitSystem.METRIC
-        }
-        
-        directionsService.route(request, (result, status) => {
-        
-            if(status == google.maps.DirectionsStatus.OK){
-                
-                const output = document.querySelector("#output");
-                
-                output.innerHTML = "From: " + document.getElementById("from").value + "<br>To: " + document.getElementById("to").value + "<br>Distance: " + result.routes[0].legs[0].distance.text + "<br>Duration: " + result.routes[0].legs[0].duration.text;
-                
-                directionsDisplay.setDirections(result);
-                
-            }
-            else{
-            
-                directionsDisplay.setDirections({ routes : []});
-
-                map.setCenter(location);
-            
-            }
-        
-        });
-        
-    }
-    
-    document.getElementById("calc").addEventListener("click", calcRoute());
-    
-    var options = {
-        types: ["address"],
-    }
-
-    var input1 = document.getElementById("from");
-    var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
-    var input2 = document.getElementById("to");
-    var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
-}
-
+    <script src="assets/js/cal-routes.js">
+        function initMap();
     </script>
     <script>
         function Logout() {
