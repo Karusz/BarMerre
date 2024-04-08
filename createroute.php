ab2edc4d028931f20ec94db974535c47165a8ed9
Karusz <112902245+Karusz@users.noreply.github.com>
2024-04-08 19:54:33 +0200

    Route create

--- createroute.php
+++ createroute.php
@@ -6,6 +6,9 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- CSS -->
+     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+     <link rel="stylesheet" href="assets/css/all-style.css">
 <title>TEST2</title>
 <style>
 		*{
@@ -22,31 +25,52 @@
 	</style>
 </head>
 <body>
-    <h1>TEST2</h1>
-    <div>
-        <?php 
-            if(!empty($barid)){
-                $found_coord = $conn->query("SELECT * FROM bars WHERE id = $barid");
-                $bar = $found_coord->fetch_assoc();
-            }
-            
-            ?>
-            <select name="bar" id="barsList">
-                    <option value="0">Valassz</option>
-                <?php
-                    $lekerdezes = "SELECT * FROM bars ORDER BY name";
-                    $found_coords = $conn->query($lekerdezes);
-                    while($coords=$found_coords->fetch_assoc()){
-                ?>
-                    <option value="<?=$coords['lat'].';'.$coords['lng']?>"> <?=$coords['name']?></option>
-                <?php } ?>
-            </select>
-            
-    </div>
-<button id="addAddress">Add Address</button>
-<button id="generateRoute">Generate</button>
-<button id="saveRoute">Save</button>
-<div id="map"></div>
+<header>
+        <h2 class="logo">BarMerre</h2>
+        <nav class="navigation">
+            <a href="tutorial.php" class="nav-a">Bemutató</a>
+            <a href="allroutes.php" class="nav-a">Útvonalak</a>
+            <a href="createroute.php" class="nav-a">Tervezés</a>
+            <button class="btnLogin dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Profil
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href=""><?= $user['username'] ?></a></li>
+                <li><a class="dropdown-item" href="myroutes.php">Saját utak</a></li>
+                <li><a class="dropdown-item" href="profilesettings.php?id=<?=$user['id']?>">Beállítások</a></li>
+                <li><a class="dropdown-item" href="mailto:barmerre@gmail.com">Kapcsolat</a></li>
+                <li><button class="dropdown-item" onclick="Logout()">Kijelentkezés</button></li>
+            </ul>
+        </nav>
+    </header>
+  <div>
+    <?php 
+      if(!empty($barid)){
+          $found_coord = $conn->query("SELECT * FROM bars WHERE id = $barid");
+          $bar = $found_coord->fetch_assoc();
+      }
+
+    ?>
+    <select name="bar" id="barsList">
+      <option value="0">Valassz</option>
+      <?php
+          $lekerdezes = "SELECT * FROM bars ORDER BY name";
+          $found_coords = $conn->query($lekerdezes);
+          while($coords=$found_coords->fetch_assoc()){
+      ?>
+        <option value="<?=$coords['lat'].';'.$coords['lng']?>"> <?=$coords['name']?></option>
+      <?php } ?>
+    </select>
+        
+  </div>
+  <div>
+    <button id="addAddress">Add Address</button>
+    <button id="generateRoute">Generate</button>
+    <button id="saveRoute">Save</button>
+  </div>
+  <div id="map"></div>
+
+
 
 <script>
     markersLatLng = [];
