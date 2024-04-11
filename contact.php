c26d882c9883997fb2e73598143cd5350c40ccf9
Karusz <112902245+Karusz@users.noreply.github.com>
2024-04-11 12:23:12 +0200

    title

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>BarMerre - Kapcsolat</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Core theme CSS (includes Bootstrap)-->
